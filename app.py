f6a3946398dd8829cadbede320e5cb7cf04376f5
Ziam Jan <46251876+ZiamJan@users.noreply.github.com>
2019-11-02 20:31:22 -0400

    Update app.py
    changed my command code

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "!test":
-    send_msg("Hello World!")
+  if data['text'].lower() == "will i get a girlfriend":
+    send_msg("Yeah Yeah Yeah Yeah")
 	
 
   elif data['text'].lower() == "!testpic":
